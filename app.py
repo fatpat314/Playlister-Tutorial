543c48885341ef2d9d58abf7b904f43dc7cad233
Patrick <w.patrick.kelly@gmail.com>
2019-09-24 16:58:25 -0700

    weird database error please help

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
 
+
 app = Flask(__name__)
 
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
 #@app.route('/')
 
 #def index():
@@ -10,16 +14,26 @@ app = Flask(__name__)
 
 # Our mock array of projects
 
-#playlists = [
-#    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-#    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' },
-#    { 'title': 'Skate videos', 'description': 'Don\'t not wear a helmet'}
-#]
+    #playlists = [
+        #{ 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+        #{ 'title': '80\'s Music', 'description': 'Don\'t stop believing!' },
+        #{ 'title': 'Skate videos', 'description': 'Don\'t not wear a helmet'}
+        #]
+'''playlists = [
+{ 'title': 'stoney street', 'artist': 'Amon Tobin' },
+{ 'title': 'glantz graf', 'artist': 'Autechre' }
+]'''
 
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists.find())
+
+@app.route('/playlists/new')
+def playlists_new():
+
+
+    return render_template('playlists_new.html')
 
 
 if __name__ == '__main__':
