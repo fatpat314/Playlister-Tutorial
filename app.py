17ed66eb7eef23a2b156e0d258ae81c5c61510a4
Patrick <w.patrick.kelly@gmail.com>
2019-09-24 10:41:07 -0700

    mongoDB installed

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
 #@app.route('/')
+
 #def index():
     #return render_template('home.html', msg='Flask is cool!!')
 
 # Our mock array of projects
 
-playlists = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-]
+#playlists = [
+#    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+#    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' },
+#    { 'title': 'Skate videos', 'description': 'Don\'t not wear a helmet'}
+#]
 
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
-    return render_template('playlists_index.html', playlists=playlists)
+    return render_template('playlists_index.html', playlists=playlists.find())
 
 
 if __name__ == '__main__':
