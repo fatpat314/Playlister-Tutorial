c26ce5b7abbdb689f6e73fc0730eb03b196ff84c
Patrick <w.patrick.kelly@gmail.com>
2019-09-28 14:42:10 -0700

    page 5 done but still does not work...

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ db = client.Playlister
 playlists = db.playlists
 
 from flask import Flask, render_template, request, redirect, url_for
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -47,10 +48,16 @@ def playlists_submit():
         'description': request.form.get('description'),
         'videos': request.form.get('videos').split()
     }
-    playlist.insert_one(playlist)
+    #playlist.insert_one(playlist)
     #print(request.form.to_dict())
-    return redirect(url_for('playlists_index'))
+    playlist_id = playlists.insert_one(playlist).inserted_id
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
 
 
 
